de09158e316dd6bfc024518971633794ba265c97
Knabbelbaard <joukedj@xs4all.nl>
2025-08-12 22:12:55 +0200

    Peer

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def index():
         adjusted=adjusted)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
